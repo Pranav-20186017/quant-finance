fb33f1263e6d6cce039f9a880cb4a73b45e33866
Pranav-20186017 <sharmapranav307@gmail.com>
2022-11-12 22:38:28 +0530

    feat: Samle Implementation of Quadtric Variation

---
+++ br.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import random
+X = [-1] * 56 + [1] * 44
+random.shuffle(X)
+M = []
+s = 0
+for i in X:
+    s += i
+    M.append(s)
+dif = 0
+for i in range(1, 5):
+    td = (M[i] - M[i - 1]) ** 2
+    dif += td
+print(i)
+print(dif)
